a8467fe733763561ba87f161499f8aa44f19d9c8
Jevin Nishioka <jevnishioka1@gmail.com>
2026-06-09 02:09:52 -0500

    Use migratable maintenance policy for GCP runners (#78)

--- bucephalus-cloud/deploy/provider/gcp/provision-runner-vm.js
+++ bucephalus-cloud/deploy/provider/gcp/provision-runner-vm.js
@@ -84,7 +84,7 @@ async function main() {
     },
     scheduling: {
       automaticRestart: false,
-      onHostMaintenance: "TERMINATE",
+      onHostMaintenance: "MIGRATE",
       provisioningModel: "STANDARD",
     },
     shieldedInstanceConfig: {
